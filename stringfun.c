6dbba0e9e8f526ceb0d9e0bab187fe28401d94a7
gomba66 <danielalejo66@hotmail.com>
2019-08-28 19:58:37 +0000

    [FIX]I deleted the salto function

--- stringfun.c
+++ stringfun.c
@@ -55,12 +55,3 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	return (dest);
 }
-/**
- * salto - Function that detect Ctr + C and print a new line
- * Return: nothing.
- */
-
-void salto(void)
-{
-	_printstring("\x1B[32m\n$ \x1B[0m");
-}
