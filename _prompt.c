e19414698baba4436a0d0e9ad3f5d8fcf6f74bc6
gomba66 <danielalejo66@hotmail.com>
2019-08-28 08:06:51 +0000

    [Fix] gcc flags

--- _prompt.c
+++ _prompt.c
@@ -1,13 +1,15 @@
 #include "holberton.h"
 /**
  * _prompt - Function that receive a string from the standart input
+ * @envp: enviroment.
  * Return: -1 if fail or a string on success
  */
-char *_prompt()
+char *_prompt(char **envp)
 {
 	char *b = NULL;
 	size_t bufsize = 0;
 	ssize_t count = 0;
+	int i = 0;
 
 	_printstring("\x1B[32m");
 	_printstring("$ ");
@@ -23,11 +25,21 @@ char *_prompt()
 		b = NULL;
 		return (b);
 	}
+	if (b[0] == 'e' && b[1] == 'n' && b[2] == 'v' && b[3] == '\n')
+	{
+		while (envp[i])
+		{
+			printf("%s\n", envp[i]);
+			i++;
+		}
+		b = NULL;
+		return (b);
+	}
 
 	if (b[0] == 'e' && b[1] == 'x' && b[2] == 'i' && b[3] == 't' && b[4] == '\n')
 	{
 		free(b);
-		exit(0);
+		exit(98);
 	}
 
 	return (b);
