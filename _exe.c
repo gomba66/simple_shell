6d40d64195f13576804e7b0a39382ea9b4c68a3c
JoseMarulanda <josejumarumol@gmail.com>
2019-08-29 18:48:17 +0000

    fix unused

--- _exe.c
+++ _exe.c
@@ -6,19 +6,11 @@
  * @av: string that contain the name of the program.
  * Return: not return on success, otherwise return -1 if fail.
  */
-void _execv(char **token, char **tokpath, char **av)
+void _execv(char **token, char **tokpath)
 {
 	pid_t pid_child = 0;
-	int j = 0, newcount = 0, x = 2;
-	char *namefile;
+	int j = 0;
 
-	for (x = 2, newcount = 0; av[0][x] != '\0'; x++, newcount++)
-		;
-	namefile = malloc(sizeof(char) * newcount);
-	for (x = 2, j = 0; av[0][x] != '\0'; x++, j++)
-		namefile[j] = av[0][x];
-	namefile[j] = '\0';
-	j = 0;
 	pid_child = fork();
 	if (pid_child < 0)
 	{
